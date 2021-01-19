0107e245b478a1d24c25266b97a1dfea5ad8873d
tasha_dexter <tasha_dexter@mail.ru>
2021-01-19 18:01:54 +0200

    Checking .gitignore

---
+++ DexStudy/PetsOop/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PetsOop
+{
+    public class Dog : Pet
+    {
+        public Dog()
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog says: \"Woof! Woof!\"");
+        }
+
+        public override void Move(int distance)
+        {
+            Console.WriteLine($"Dog ran {distance} meters");
+        }
+    }
+}
