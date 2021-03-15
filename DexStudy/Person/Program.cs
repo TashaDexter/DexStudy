5ad62070667f8d0568c4f301f8c936863064ddcc
tasha_dexter <tasha_dexter@mail.ru>
2021-03-15 13:34:52 +0200

    Fixed Person, leaved only 1 comparison

--- DexStudy/Person/Program.cs
+++ DexStudy/Person/Program.cs
@@ -16,32 +16,17 @@ namespace Person
     {
         static void Main(string[] args)
         {
-            string s1 = "Петр Васильевич";
-            Person p1 = (Person)s1;
-            Console.WriteLine("Строка:" + s1 + "\nЯвное преобразование в Person: FirstName - " + p1.FirstName + ", LastName - " + p1.LastName);
-            Console.WriteLine("Сравнение при помощи ==: " + (s1 == p1));
-            Console.WriteLine("Сравнение при помощи Equals: " + s1.Equals(p1));
-            Console.WriteLine();
+            Person p1 = new Person();
+            p1.FirstName = "Петр";
+            p1.LastName = "Васильевич";
 
-            string s2 = "Евгения Николаевна";
-            Person p2 = s2;            
-            Console.WriteLine("Строка:" + s2 + "\nНеявное преобразование в Person: FirstName - " + p2.FirstName + ", LastName - " + p2.LastName);
-            Person p3 = new Person { FirstName = "Николай", LastName = "Викторович" };
-            Console.WriteLine("Сравнение при помощи ==: " + (s2 == p2));
-            Console.WriteLine("Сравнение при помощи Equals: " + s2.Equals(p2));
-            Console.WriteLine();
+            string s1 = "Петр Васильевич";
             
-            string s3 = (string)p3;
-            Console.WriteLine("Person: FirstName - " + p3.FirstName + ", LastName - " + p3.LastName + "\nЯвное преобразование в строку: " + s3);
-            Console.WriteLine("Сравнение при помощи ==: " + (s3 == p3));
-            Console.WriteLine("Сравнение при помощи Equals: " + s3.Equals(p3));
-            Console.WriteLine();
-            
-            string s4 = p3;
-            Console.WriteLine("Person: FirstName - " + p3.FirstName + ", LastName - " + p3.LastName + "\nНеявное преобразование в строку: " + s3);
-            Console.WriteLine("Сравнение при помощи ==: " + (s4 == p3));
-            Console.WriteLine("Сравнение при помощи Equals: " + s4.Equals(p3));
-            Console.WriteLine();
+            Console.WriteLine("Сравнение при помощи ==: " + (s1 == p1));
+            //при сравнении происходит неявное преобразование p1 в string, оба экземпляра строки
+            //имеют одинаковую длину и идентичные символы в каждой позиции символа
+            Console.WriteLine("Сравнение при помощи Equals: " + s1.Equals(p1));
+            //в методе Equals p1 явно приводится к string, затем объекты сравниваются по имени
             Console.Read();
         }
     }
