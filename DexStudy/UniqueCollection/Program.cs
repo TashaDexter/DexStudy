2eb8ede05af1dc9d185471b9296fa90eb678c136
tasha_dexter <tasha_dexter@mail.ru>
2021-03-31 15:24:25 +0300

    Added UniqueCollection

---
+++ DexStudy/UniqueCollection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUniqueCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueCollection<int> int_collection = new UniqueCollection<int>();
+            try
+            {
+                int_collection.Add(5);
+                int_collection.Add(6);
+                int_collection.Add(7);
+                int_collection.Add(5);
+                int_collection.Add(9);
+            }
+
+            catch (Exception any)
+            {
+                Console.WriteLine(any.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Элементы коллекции:");
+                foreach (int i in int_collection)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ DexStudy/UniqueCollection/UniqueCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MyUniqueCollection
+{
+    class UniqueCollection <T> : ICollection <T>
+    {
+        private readonly ICollection<T> uniqueCol;
+        public UniqueCollection()
+        {
+            uniqueCol = new List<T>();
+        }
+
+        public bool Contains(T item)
+        {
+            return uniqueCol.Contains(item);
+        }
+
+        public void Add(T item)
+        {
+
+            if (!Contains(item))
+            {
+                uniqueCol.Add(item);
+            }
+            else
+            {
+                throw new Exception("Невозможно добавить объект "+item+". Данный элемент уже существует в коллекции!");
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            uniqueCol.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return uniqueCol.Count;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            return uniqueCol.Remove(item);
+        }
+
+        public void Clear()
+        {
+            uniqueCol.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return uniqueCol.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return uniqueCol.GetEnumerator();
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+    }
+}
