5fc04ad2f99a2ce4b527d49bcf2e6bf8cea29ece
tasha_dexter <tasha_dexter@mail.ru>
2021-03-31 14:08:13 +0300

    Added Typed dictionary

---
+++ DexStudy/Typed dictionary/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Typed_dictionary
+{
+    class Person
+    {
+        public int PassportID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null)
+            {
+                if (obj is Person)
+                {
+                    var person = (Person)obj;
+                    return person.PassportID == PassportID
+                        && person.FirstName == FirstName
+                        && person.LastName == LastName
+                        && person.BirthDate == BirthDate
+                        && person.BirthPlace == BirthPlace;
+                }
+                else return false;
+
+            }
+            else return false;
+        }
+        public override int GetHashCode()
+        {
+            return (PassportID.GetHashCode() + FirstName.GetHashCode()) % (LastName.GetHashCode() + BirthDate.GetHashCode() + BirthPlace.GetHashCode());
+        }
+
+        //Для того, чтобы при сравнении через оператор == вызвался переопределённый метод Equals
+        //необходимо перегрузить операторы отношения == и !=
+        public static bool operator ==(Person p1, Person p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !p1.Equals(p2);
+        }
+
+        public static implicit operator string(Person person)
+        {
+            return person.FirstName + " " + person.LastName+" "+ person.PassportID+ " "+ person.BirthDate.ToShortDateString()+ " "+person.BirthPlace;
+        }
+    }
+}
---
+++ DexStudy/Typed dictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typed_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Person, string> workplaces = new Dictionary<Person, string>();
+            Person p1 = new Person();
+            p1.FirstName = "Николай";
+            p1.LastName = "Петров";
+            p1.PassportID = 125879;
+            p1.BirthDate = Convert.ToDateTime("14.03.91");
+            p1.BirthPlace = "Бендеры";
+
+            Person p2 = new Person();
+            p2.FirstName = "Василий";
+            p2.LastName = "Николаев";
+            p2.PassportID = 125574;
+            p2.BirthDate = Convert.ToDateTime("25.06.85");
+            p2.BirthPlace = "Тирасполь";
+
+            Person p3 = new Person();
+            p3.FirstName = "Кристина";
+            p3.LastName = "Яковлева";
+            p3.PassportID = 845714;
+            p3.BirthDate = Convert.ToDateTime("24.10.76");
+            p3.BirthPlace = "Тирасполь";
+
+            workplaces.Add(p1, "Инженерно-технический институт");
+            workplaces.Add(p2, "Гуманитарно-математическая гимназия");
+            workplaces.Add(p3, "Хлопчато-бумажный комбинат");
+
+            Console.WriteLine("Содеражние справочника workplaces:");
+            foreach (var p in workplaces)
+                Console.WriteLine(p.Key + " - " + p.Value);
+
+            Person person = new Person();
+            Console.WriteLine("\nВведите данные о физическом лице:");
+            Console.Write("Имя:");
+            person.FirstName = Console.ReadLine();
+            Console.Write("Фамилия:");
+            person.LastName = Console.ReadLine();
+            Console.Write("Номер паспорта:");
+            person.PassportID= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Дата рождения:");
+            person.BirthDate = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Место рождения:");
+            person.BirthPlace = Console.ReadLine();
+
+            if (workplaces.ContainsKey(person))
+            {
+                Console.WriteLine("Место работы:"+workplaces[person]);
+            }
+            else
+                Console.WriteLine("Данное физическое лицо отсутствует в базе данных!");
+
+            Console.ReadLine();
+        }
+    }
+}
