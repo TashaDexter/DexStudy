d8e38bb1ba8d343c83cb979ff66c28abbdfcf9ea
tasha_dexter <tasha_dexter@mail.ru>
2021-04-05 13:34:55 +0300

    Added class Person in MyUniqueCollection

---
+++ DexStudy/UniqueCollection/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MyUniqueCollection
+{
+    class Person
+    {
+        public int PassportID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null)
+            {
+                if (obj is Person)
+                {
+                    var person = (Person)obj;
+                    return person.PassportID == PassportID
+                        && person.FirstName == FirstName
+                        && person.LastName == LastName
+                        && person.BirthDate == BirthDate
+                        && person.BirthPlace == BirthPlace;
+                }
+                else return false;
+
+            }
+            else return false;
+        }
+        public override int GetHashCode()
+        {
+            return (PassportID.GetHashCode() + FirstName.GetHashCode()) % (LastName.GetHashCode() + BirthDate.GetHashCode() + BirthPlace.GetHashCode());
+        }
+
+        //Для того, чтобы при сравнении через оператор == вызвался переопределённый метод Equals
+        //необходимо перегрузить операторы отношения == и !=
+        public static bool operator ==(Person p1, Person p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !p1.Equals(p2);
+        }
+
+        //перегрузка операций преобразования в тип String
+        public static implicit operator Person(string s)
+        {
+            string[] initials = s.Split(' ');
+            return new Person { FirstName = initials[0], LastName = initials[1] };
+        }
+
+        public static implicit operator string(Person person)
+        {
+            return person.FirstName + " " + person.LastName;
+        }
+    }
+}
--- DexStudy/UniqueCollection/Program.cs
+++ DexStudy/UniqueCollection/Program.cs
@@ -13,14 +13,42 @@ namespace MyUniqueCollection
         //при попытке добавить существующий инстанс, выбрасывает исключение
         static void Main(string[] args)
         {
+
             UniqueCollection<int> int_collection = new UniqueCollection<int>();
+            UniqueCollection<Person> p_collection = new UniqueCollection<Person>();
             try
             {
                 int_collection.Add(5);
+                int_collection.Add(4);
+                int_collection.Add(3);
                 int_collection.Add(6);
-                int_collection.Add(7);
-                int_collection.Add(5);
-                int_collection.Add(9);
+                //int_collection.Add(5);
+                int_collection.Add(14);
+
+                Person p1 = new Person();
+                p1.FirstName = "Мария";
+                p1.LastName = "Иванова";
+                p1.PassportID = 15825;
+                p1.BirthDate = Convert.ToDateTime("18.05.2010");
+                p1.BirthPlace = "Первомайск";
+
+                Person p2 = new Person();
+                p2.FirstName = "Оксана";
+                p2.LastName = "Иванова";
+                p2.PassportID = 15625;
+                p2.BirthDate = Convert.ToDateTime("18.05.2010");
+                p2.BirthPlace = "Первомайск";
+
+                Person p3 = new Person();
+                p3.FirstName = "Мария";
+                p3.LastName = "Иванова";
+                p3.PassportID = 15825;
+                p3.BirthDate = Convert.ToDateTime("18.05.2010");
+                p3.BirthPlace = "Первомайск";
+
+                p_collection.Add(p1);
+                p_collection.Add(p2);
+                p_collection.Add(p3);
             }
 
             catch (Exception any)
@@ -30,10 +58,15 @@ namespace MyUniqueCollection
 
             finally
             {
-                Console.WriteLine("Элементы коллекции:");
+                Console.WriteLine("\nКоллекция объектов int:");
                 foreach (int i in int_collection)
                 {
                     Console.WriteLine(i);
+                }
+                Console.WriteLine("\nКоллекция объектов Person:");
+                foreach (Person p in p_collection)
+                {
+                    Console.WriteLine(p);
                 }
                 Console.ReadLine();
             }
